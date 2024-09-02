c3076cd612d2bc291536b6145ef28381d0752567
khrisnaa <igdkhrisnaaa@gmail.com>
2024-09-02 13:04:13 +0800

    add about us section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { About } from '@/components/about';
 import { Home } from '@/components/home';
 import { FeaturedShoesSection } from '@/components/home/featured-shoes-section';
 import { UserButton } from '@clerk/nextjs';
@@ -8,6 +9,7 @@ export default function Page() {
       {/* <div className="h-screen bg-slate-300"></div> */}
       <Home />
       <FeaturedShoesSection />
+      <About />
       <div className="h-screen bg-slate-300"></div>
       <div className="h-screen bg-slate-500"></div>
     </main>
