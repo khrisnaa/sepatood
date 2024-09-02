de75ab424f7d7b6b9da3a1607d805d051dfcd5a8
khrisnaa <igdkhrisnaaa@gmail.com>
2024-09-02 21:05:18 +0800

    add brand list

--- app/(root)/(public)/home/page.tsx
+++ app/(root)/(public)/home/page.tsx
@@ -1,14 +1,18 @@
+import { BrandList } from '@/app/(root)/(public)/home/_components/brand-list';
 import { BillboardCarousel } from './_components/billboard-carousel';
 import { Hero } from './_components/hero';
 
 const Page = () => {
   return (
-    <main>
+    <main className="space-y-4">
       <div>
         <Hero />
       </div>
       <div>
         <BillboardCarousel />
+      </div>
+      <div>
+        <BrandList />
       </div>
       <div className="h-screen bg-slate-300"></div>
     </main>
